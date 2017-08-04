ac422a97151eabf533987bd1a465e0808424ddde
Alan Russell <ajrussellaudio@gmail.com>
2017-08-04 16:36:29 +0100

    implement button

--- js/app.js
+++ js/app.js
@@ -1,12 +1,30 @@
-function playSound() {
-  console.log("Mrrrrrrr!!")
-}
-
-function buildButton() {
+function buildButton(playSound) {
   var button = document.createElement("button");
   button.textContent = "Airhorn!";
   button.addEventListener('click', playSound);
   document.body.appendChild(button);
 }
 
-window.addEventListener('load', buildButton);
+function loadAudio() {
+  var context = new AudioContext();
+  var request = new XMLHttpRequest();
+  request.open("GET", "samples/airhorn.mp3");
+  request.responseType = "arraybuffer";
+  request.addEventListener('load', function() {
+    context.decodeAudioData(this.response, function(buffer) {
+      buildButton(function() {
+        var source = context.createBufferSource();
+        source.buffer = buffer;
+        source.connect(context.destination);
+        source.start(0);
+      });
+    }, onSoundLoadError)
+  });
+  request.send();
+}
+
+function onSoundLoadError() {
+  console.log("ERROR loading sounds!");
+}
+
+window.addEventListener('load', loadAudio);
